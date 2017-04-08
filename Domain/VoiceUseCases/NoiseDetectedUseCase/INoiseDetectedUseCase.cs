9bc53f739f97c085a47d234563339fbf10793959
Cem Philipp Freimoser <freimoser.c@gmail.com>
2017-04-08 23:01:49 +0200

    SNOW-UC-ND added Link to Documentation

--- Domain/VoiceUseCases/NoiseDetectedUseCase/INoiseDetectedUseCase.cs
+++ Domain/VoiceUseCases/NoiseDetectedUseCase/INoiseDetectedUseCase.cs
@@ -2,6 +2,7 @@
 {
     using System.Threading.Tasks;
 
+    // UseCase https://docs.google.com/document/d/1yUpelTJoRxF9umwHOJSW4e-bFlqAic3YuWJPYUUCpoc/edit?usp=sharing
     public interface INoiseDetectedUseCase
     {
         Task OnNoiseDetected(NoiseDetectedRequest noiseDetectedRequest);
