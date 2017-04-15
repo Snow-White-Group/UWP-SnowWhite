33eea9e0f0eadeef252056eba795fa144bb9aa4c
Cem Philipp Freimoser <freimoser.c@gmail.com>
2017-04-15 16:24:40 +0200

    Created coustom crontoll for date and time
    Related Work Items: #60

---
+++ Snowwhite/Controls/ClockControl/MyClockControl.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.System.Threading;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace Snowwhite.Controls.ClockControl
+{
+    public sealed partial class MyClockControl : UserControl
+    {
+        public static string Fdate { get; } = " ddd, MM yyyy";
+        public static string Ftime { get; } = "hh:mm:ss";
+
+        public MyClockControl()
+        {
+            this.InitializeComponent();
+            
+            Time = DateTime.Now.ToString(Ftime);
+            Date = DateTime.Now.ToString(Fdate);
+
+            AutoUpdate();
+        }
+
+        public string Time
+        {
+            get => (string) GetValue(TimeProperty);
+            set => SetValue(TimeProperty,value);
+        }
+
+        public string Date
+        {
+            get => (string) GetValue(DateProperty);
+            set => SetValue(DateProperty, value);
+        }
+
+        public static readonly DependencyProperty TimeProperty =
+            DependencyProperty.Register("time", typeof(string), typeof(MyClockControl), null);
+        public static readonly DependencyProperty DateProperty =
+            DependencyProperty.Register("date", typeof(string), typeof(MyClockControl), null);
+
+        private void AutoUpdate()
+        {
+            var period = TimeSpan.FromSeconds(1);
+            ThreadPoolTimer.CreatePeriodicTimer((source) =>
+            {
+                Dispatcher?.RunAsync(CoreDispatcherPriority.High,
+                    () =>
+                    {
+                        Time = DateTime.Now.ToString(Ftime);
+                        Date = DateTime.Now.ToString(Fdate);
+                    });
+            }, period);
+        }
+    }
+}
