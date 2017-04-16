2b50589cf4e4fff2987f568eeabcb3568f7c2148
Cem Philipp Freimoser <freimoser.c@gmail.com>
2017-04-16 23:27:59 +0200

    clockDwarf code clean up NewsDwarf model has been created. Open points: do we need a source prop?
    Related Work Items: #60

--- DwarfLibrary/ClockDwarf/ClockDwarfControl.xaml.cs
+++ DwarfLibrary/ClockDwarf/ClockDwarfControl.xaml.cs
@@ -10,35 +10,38 @@ namespace DwarfLibrary.ClockDwarf
 {
     public sealed partial class ClockDwarfControl : UserControl
     {
+        #region public fields
         public static string Fdate { get; } = " ddd, MM. yyyy";
         public static string Ftime { get; } = "hh:mm:ss";
+        public string Time
+        {
+            get => (string)GetValue(TimeProperty);
+            set => SetValue(TimeProperty, value);
+        }
+        public string Date
+        {
+            get => (string)GetValue(DateProperty);
+            set => SetValue(DateProperty, value);
+        }
         public static readonly DependencyProperty TimeProperty =
             DependencyProperty.Register("time", typeof(string), typeof(ClockDwarfControl), null);
         public static readonly DependencyProperty DateProperty =
             DependencyProperty.Register("date", typeof(string), typeof(ClockDwarfControl), null);
+        #endregion
 
+        #region constuctor
         public ClockDwarfControl()
         {
             this.InitializeComponent();
-            
+
             Time = DateTime.Now.ToString(Ftime);
             Date = DateTime.Now.ToString(Fdate);
 
             AutoUpdate();
         }
+        #endregion
 
-        public string Time
-        {
-            get => (string) GetValue(TimeProperty);
-            set => SetValue(TimeProperty,value);
-        }
-
-        public string Date
-        {
-            get => (string) GetValue(DateProperty);
-            set => SetValue(DateProperty, value);
-        }
-
+        #region function
         private void AutoUpdate()
         {
             var period = TimeSpan.FromSeconds(1);
@@ -52,5 +55,6 @@ namespace DwarfLibrary.ClockDwarf
                     });
             }, period);
         }
+        #endregion
     }
 }
---
+++ DwarfLibrary/NewsDwarf/NewsModel.cs
@@ -0,0 +1,9 @@
+﻿namespace DwarfLibrary.NewsDwarf
+{
+    public class NewsModel
+    {
+        public string Headline { get; set; }
+        public string ShortLine { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
