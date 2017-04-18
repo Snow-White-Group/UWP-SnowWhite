67d97a62428b24c7614dee0f75799f5d13226eac
Cem Philipp Freimoser <freimoser.c@gmail.com>
2017-04-19 00:38:27 +0200

    working on NewsDwarf
    Related Work Items: #60

---
+++ DwarfLibrary/NewsDwarf/NewsDwarfControl.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace DwarfLibrary.NewsDwarf
+{
+    public sealed partial class NewsDwarfControl : UserControl
+    {
+        #region public fields
+        public List<NewsModel> News
+        {
+            get => (List<NewsModel>)GetValue(NewsProperty);
+            set => SetValue(NewsProperty, value);
+        }
+        public static readonly DependencyProperty NewsProperty =
+            DependencyProperty.Register("news", typeof(List<NewsModel>), typeof(NewsDwarfControl), null);
+        #endregion
+
+        public NewsDwarfControl()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
--- DwarfLibrary/NewsDwarf/NewsModel.cs
+++ DwarfLibrary/NewsDwarf/NewsModel.cs
@@ -5,5 +5,14 @@
         public string Headline { get; set; }
         public string ShortLine { get; set; }
         public string ImageUrl { get; set; }
+        public string Source { get; set; }
+
+        public NewsModel(string headline, string shortLine, string imageUrl, string source)
+        {
+            Headline = headline;
+            ShortLine = shortLine;
+            ImageUrl = imageUrl;
+            Source = source;
+        }
     }
 }
