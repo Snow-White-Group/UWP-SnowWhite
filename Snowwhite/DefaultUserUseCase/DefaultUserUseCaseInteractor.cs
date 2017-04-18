b968d01bf461bd34ae9ac7b6a4789291d0c801ed
Dominik Juelg <djuelg@gmx.de>
2017-04-18 07:41:39 +0200

    first boilerplate files for review

---
+++ Snowwhite/DefaultUserUseCase/DefaultUserUseCaseInteractor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class DefaultUserUseCaseInteractor : IDefaultUserUseCase
+{
+    private readonly IConfigurationPageService _configurationPageService;
+    private readonly IWeatherService _weatherService;
+
+    public void TriggerDefaultUser()
+    {
+        // load user and weather
+        // return data for boundary for presenter (http://i.imgur.com/WkBAATy.png)
+    }
+}
---
+++ Snowwhite/DefaultUserUseCase/IDefaultUserUseCase.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+/// see https://docs.google.com/document/d/1FWYv4QkBiAoUlHTx3b8KAfjPPePmy4bHFGYrH55wusQ/edit#
+public interface IDefaultUserUseCase
+{
+    void TriggerDefaultUser();
+}
