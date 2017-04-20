d2baf38e2d847dbf1c103a8d7076b35ea6b824a3
Mario Kunstek <kunstek.mario@googlemail.com>
2017-04-20 10:49:07 +0200

    Added a web service which has the http client for sendeing requests and get responses
    Verknüpfte Arbeitselemente: #85

---
+++ ServicesGateways/WebService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServicesGateways
+{
+    public class WebService
+    {
+        // http client with timeout
+        private static readonly HttpClient httpClient = new HttpClient(new HttpClientHandler())
+        {
+            Timeout = TimeSpan.FromSeconds(5)
+        };
+
+        public async Task<string> MakeRequest(string url) {
+            // request => return json response
+            HttpResponseMessage response = await httpClient.GetAsync(url);
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
