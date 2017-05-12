9d46290352268dbb002510efaffd4ce17520fa34
Cem Philipp Freimoser <cfreimoser@united-internet.de>
2017-05-12 20:54:29 +0200

    first ui test
    Related Work Items: #136

---
+++ Snowwhite.Test/LoadingScreenTest.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UITesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UITest.Input;
+using Microsoft.VisualStudio.TestTools.UITest.Extension;
+using Microsoft.VisualStudio.TestTools.UITesting.HtmlControls;
+using Microsoft.VisualStudio.TestTools.UITesting.DirectUIControls;
+using Microsoft.VisualStudio.TestTools.UITesting.WindowsRuntimeControls;
+using Keyboard = Microsoft.VisualStudio.TestTools.UITesting.Keyboard;
+
+
+namespace Snowwhite.Test
+{
+    /// <summary>
+    /// Zusammenfassungsbeschreibung für CodedUITest1
+    /// </summary>
+    [CodedUITest(CodedUITestType.WindowsStore)]
+    public class LoadingScreenTest
+    {
+        public LoadingScreenTest()
+        {
+        }
+
+        [TestMethod]
+        public void CodedUITestMethod1()
+        {
+            XamlWindow.Launch("d131933c-490a-4b6b-ac88-cf9b43fd999a_5v9vtwr4m7rda!App");
+            this.UIMap.CheckStartUpText();
+        }
+
+        #region Zusätzliche Testattribute
+
+        // Sie können beim Schreiben der Tests folgende zusätzliche Attribute verwenden:
+
+        ////Verwenden Sie "TestInitialize", um vor dem Ausführen der einzelnen Tests Code auszuführen 
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{        
+        //    // Wählen Sie zum Generieren von Code für den Test im Kontextmenü "Code für Coded UI-Test generieren" aus, und wählen Sie eine der Menüelemente aus.
+        //}
+
+        ////Verwenden Sie "TestCleanup", um nach dem Ausführen der einzelnen Tests Code auszuführen
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{        
+        //    // Wählen Sie zum Generieren von Code für den Test im Kontextmenü "Code für Coded UI-Test generieren" aus, und wählen Sie eine der Menüelemente aus.
+        //}
+
+        #endregion
+
+        /// <summary>
+        ///Ruft den Textkontext mit Informationen über
+        ///den aktuellen Testlauf sowie Funktionalität für diesen auf oder legt diese fest.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+        private TestContext testContextInstance;
+
+        public UIMap UIMap
+        {
+            get
+            {
+                if ((this.map == null))
+                {
+                    this.map = new UIMap();
+                }
+
+                return this.map;
+            }
+        }
+
+        private UIMap map;
+    }
+}
---
+++ Snowwhite.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgende 
+// Attributgruppe gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("Snowwhite.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Snowwhite.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf "false" werden die Typen in dieser Assembly 
+// für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von 
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID ist für die ID der typelib, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("52750545-61a1-42f3-a9e5-864271a54830")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion 
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
+// übernehmen, indem Sie "*" eingeben:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
