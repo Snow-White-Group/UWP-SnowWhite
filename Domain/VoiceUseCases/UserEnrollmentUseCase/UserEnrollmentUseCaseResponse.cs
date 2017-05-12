1453c865d685cf2290b6644ecc9b621ec05b222f
Cem Philipp Freimoser <cfreimoser@united-internet.de>
2017-05-12 23:48:34 +0200

    the previous commit

--- Domain/VoiceUseCases/UserEnrollmentUseCase/UserEnrollmentUseCaseResponse.cs
+++ Domain/VoiceUseCases/UserEnrollmentUseCase/UserEnrollmentUseCaseResponse.cs
@@ -5,12 +5,21 @@ namespace Domain.VoiceUseCases.UserEnrollmentUseCase
     public class UserEnrollmentUseCaseResponse
     {
         public readonly int RemainingEnrollments;
-
-        public readonly EnrollmentState LastEnrollmentState;
-
+        public readonly EnrollmentState PreviousEnrollmentState;
         public readonly SnowUser UserToEnroll;
+        public readonly string EnrollmentPharse;
 
-        
+        public UserEnrollmentUseCaseResponse(
+            int remainingEnrollments, 
+            EnrollmentState previousEnrollmentState,
+            SnowUser userToEnroll, 
+            string enrollmentPharse)
+        {
+            this.RemainingEnrollments = remainingEnrollments;
+            this.PreviousEnrollmentState = previousEnrollmentState;
+            this.UserToEnroll = userToEnroll;
+            this.EnrollmentPharse = enrollmentPharse;
+        }
     }
 
     public enum EnrollmentState
