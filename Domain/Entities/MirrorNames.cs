b38edc20fb59f15aac4041c13ae59e95c0b3ab84
Mario Kunstek <kunstek.mario@googlemail.com>
2017-06-21 12:26:54 +0200

    Added a service for the connection with the coonfigurationpage
    Related Work Items: #185

---
+++ Domain/Entities/MirrorNames.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class MirrorNames
+    {
+        public string DisplayName;
+        public string SecretName;
+    }
+}
---
+++ ServicesGateways/HandshakeService.cs
@@ -0,0 +1,39 @@
+﻿using Domain.Entities;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServicesGateways
+{
+    public class HandshakeService
+    {
+        // instantiate the service for making requests and get responses
+        private static readonly WebService service = new WebService();
+
+        public async Task<MirrorNames> GetMirrorNames()
+        {
+            var mirrorNamesResponse = await service.MakeRequest("http://snowwhite-configurationpage.azurewebsites.net/Configuration/GenerateMirrorNames");
+            var mirrorNames = JsonConvert.DeserializeObject<MirrorNames>(mirrorNamesResponse);
+
+            return mirrorNames;
+        }
+
+        public async Task GetPostfach(string mirrorId)
+        {
+            var postfachResponse = await service.MakeRequest("http://snowwhite-configurationpage.azurewebsites.net/Configuration/GetPostbox?id=" + mirrorId);
+            // user model is different and no mirror model
+            // var postfach = JsonConvert.DeserializeObject<Actions>(postfachResponse);
+
+            // return List<Action>;
+        }
+
+        public async Task MakeHandshake(string secretName)
+        {
+            var postfachResponse = await service.MakeRequest("http://snowwhite-configurationpage.azurewebsites.net/Configuration/Handshake?secretname=" + secretName);
+            // what here?
+        }
+    }
+}
